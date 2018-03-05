df79022cb98c7d8ecf392ff6b58f0d9cd32060ea
Tim 'mithro' Ansell <me@mith.ro>
2018-03-04 17:43:20 -0800

    debian: Some install stuff.

--- vtr/debian/buildenv.sh
+++ vtr/debian/buildenv.sh
@@ -3,10 +3,14 @@
 set -e
 set -x
 
-export CC=clang
-export CXX=clang++
+#export CC=gcc
+#export CXX=g++
 #export BUILD_TYPE=debug
 export CMAKE_PARAMS="-DCMAKE_INSTALL_PREFIX=/opt/vtr"
 
 cd /workspace/vtr-verilog-to-routing
 make -j8
+
+make install
+mkdir /opt/vtr/lib
+mv /opt/vtr/bin/*.a /opt/vtr/lib/
